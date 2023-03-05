f6e98f73eebc1576a5cb0e4f86e301824fc3b6ac
Ineffable22 <miguel_grillo_orellana@hotmail.com>
2023-03-05 13:31:45 -0500

    Feat(Update): add EC_GROUP_free if failure

--- crypto/ec_to_pub.c
+++ crypto/ec_to_pub.c
@@ -34,11 +34,12 @@ uint8_t *ec_to_pub(EC_KEY const *key, uint8_t pub[EC_PUB_LEN])
 		return (NULL);
 	}
 	len = EC_POINT_point2oct(info_curve, point_curve,
-				POINT_CONVERSION_UNCOMPRESSED, pub, EC_PUB_LEN, NULL);
+				 POINT_CONVERSION_UNCOMPRESSED, pub, EC_PUB_LEN, NULL);
 	if (len == 0)
 	{
 		fprintf(stderr, "EC_POINT_point2oct failed\n");
 		EC_KEY_free((EC_KEY *)key);
+		EC_GROUP_free((EC_GROUP *)info_curve);
 		return (NULL);
 	}
 	return (pub);
