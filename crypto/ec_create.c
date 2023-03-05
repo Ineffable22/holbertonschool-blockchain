1e61ce259b8e85e0e20526c5c8976587440cb236
Ineffable22 <miguel_grillo_orellana@hotmail.com>
2023-03-05 13:09:28 -0500

    Feat(Betty): Style corrected

--- crypto/ec_create.c
+++ crypto/ec_create.c
@@ -9,7 +9,7 @@
 EC_KEY *ec_create(void)
 {
 	EC_KEY *key;
-	
+
 	key = EC_KEY_new_by_curve_name(EC_CURVE);
 	if (key == NULL)
 		return (NULL);
@@ -17,7 +17,7 @@ EC_KEY *ec_create(void)
 	{
 		fprintf(stderr, "EC_KEY_generate_key failed\n");
 		EC_KEY_free(key);
-        	return (NULL);
+		return (NULL);
 	}
 	return (key);
 }
