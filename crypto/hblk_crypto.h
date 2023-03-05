2e7e856d91010ecde9916d0fcf57e6b423ee268e
Ineffable22 <miguel_grillo_orellana@hotmail.com>
2023-03-05 14:36:24 -0500

    Feat(Update): New prototype added

--- crypto/hblk_crypto.h
+++ crypto/hblk_crypto.h
@@ -5,7 +5,9 @@
 #include <openssl/sha.h>
 #include <openssl/ec.h>
 #include <openssl/obj_mac.h>
+#include <openssl/bn.h>
 #include <stdint.h>
+#include <string.h>
 
 /* This constant is the numerical identifier of the SECP256K1 elliptic curve */
 /* cryptography standard */
@@ -36,8 +38,11 @@ typedef struct sig_s
 	uint8_t len;
 } sig_t;
 
+void _print_hex_buffer(uint8_t const *buf, size_t len);
 uint8_t *sha256(int8_t const *s, size_t len,
 		uint8_t digest[SHA256_DIGEST_LENGTH]);
 EC_KEY *ec_create(void);
+uint8_t *ec_to_pub(EC_KEY const *key, uint8_t pub[EC_PUB_LEN]);
+EC_KEY *ec_from_pub(uint8_t const pub[EC_PUB_LEN]);
 
 #endif /* _HBLK_CRYPTO_H_ */
