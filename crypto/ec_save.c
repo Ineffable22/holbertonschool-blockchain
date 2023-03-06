f3d1da19922069d3575074d86013945733900476
Ineffable22 <miguel_grillo_orellana@hotmail.com>
2023-03-06 18:19:27 -0500

    Add: New file ec_save.c

---
+++ crypto/ec_save.c
@@ -0,0 +1,34 @@
+#include "hblk_crypto.h"
+
+/**
+ * ec_save - Saves an existing EC key pair on the disk
+ * @key:    Points to the EC key pair to be saved on disk
+ * @folder: Iis the path to the folder in which to save the keys
+ *
+ * Return: 1 or 0 upon success or failure
+ */
+int ec_save(EC_KEY *key, char const *folder)
+{
+	FILE *fp = NULL;
+	char path[1024] = {0};
+
+	if (!key || !folder)
+		return (0);
+	if (mkdir(folder, S_IRWXU | S_IRWXG | S_IRWXO))
+		return (0);
+	sprintf(path, "%s/%s", folder, PRI_FILENAME);
+	fp = fopen(path, "w");
+	if (!fp)
+		return (0);
+	if (!PEM_write_ECPrivateKey(fp, key, NULL, NULL, 0, NULL, NULL))
+		return (0);
+	fclose(fp);
+	sprintf(path, "%s/%s", folder, PUB_FILENAME);
+	fp = fopen(path, "w");
+	if (!fp)
+		return (0);
+	if (!PEM_write_EC_PUBKEY(fp, key))
+		return (0);
+	fclose(fp);
+	return (1);
+}
