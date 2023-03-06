8137510943d7eb7b36921bed7a831e14446b79f2
Ineffable22 <miguel_grillo_orellana@hotmail.com>
2023-03-06 18:19:58 -0500

    Feat(Task4): New test added, header Update

--- crypto/hblk_crypto.h
+++ crypto/hblk_crypto.h
@@ -5,10 +5,12 @@
 #include <openssl/sha.h>
 #include <openssl/ec.h>
 #include <openssl/obj_mac.h>
+#include <openssl/pem.h>
 
 #include <stdio.h>
 #include <stdint.h>
 #include <string.h>
+#include <sys/stat.h>
 
 /* This constant is the numerical identifier of the SECP256K1 elliptic curve */
 /* cryptography standard */
@@ -45,5 +47,6 @@ uint8_t *sha256(int8_t const *s, size_t len,
 EC_KEY *ec_create(void);
 uint8_t *ec_to_pub(EC_KEY const *key, uint8_t pub[EC_PUB_LEN]);
 EC_KEY *ec_from_pub(uint8_t const pub[EC_PUB_LEN]);
+int ec_save(EC_KEY *key, char const *folder);
 
 #endif /* _HBLK_CRYPTO_H_ */
