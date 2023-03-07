784c9a85f787abec34610dbea25d75880aae9a03
Ineffable22 <miguel_grillo_orellana@hotmail.com>
2023-03-06 19:47:51 -0500

    Feat(Task5): ec_load implemented

---
+++ crypto/ec_load.c
@@ -0,0 +1,36 @@
+#include "hblk_crypto.h"
+
+/**
+ * ec_load - Loads an EC key pair from the disk
+ * @folder: Is the path to the folder from which to load the keys
+ *
+ * Return: A pointer to the created EC key pair upon success,
+ * or NULL upon failure
+ */
+EC_KEY *ec_load(char const *folder)
+{
+	FILE *fp = NULL;
+	char path[1024] = {0};
+	EC_KEY *key = NULL;
+
+	if (!folder)
+		return (NULL);
+	sprintf(path, "%s/%s", folder, PRI_FILENAME);
+	fp = fopen(path, "r");
+	if (!fp)
+		return (NULL);
+	key = PEM_read_ECPrivateKey(fp, &key, NULL, NULL);
+	if (!key)
+		return (NULL);
+	fclose(fp);
+
+	sprintf(path, "%s/%s", folder, PUB_FILENAME);
+	fp = fopen(path, "r");
+	if (!fp)
+		return (NULL);
+	key = PEM_read_EC_PUBKEY(fp, &key, NULL, NULL);
+	if (!key)
+		return (NULL);
+	fclose(fp);
+	return (key);
+}
