e6e100fb782499ec805cc9248feda3edcad7dd9e
Ineffable22 <miguel_grillo_orellana@hotmail.com>
2023-03-15 00:47:48 -0500

    Feat(Task7): block_is_valid function created

---
+++ blockchain/v0.1/block_is_valid.c
@@ -0,0 +1,37 @@
+#include "blockchain.h"
+
+/**
+ * block_is_valid - Verifies that a Block is valid
+ * @block:      Points to the Block to check
+ * @prev_block: Points to the previous Block in the Blockchain,
+ *              or is NULL if block is the first Block of the chain
+ * Return: 1 if valid, or 0 otherwise
+ */
+int block_is_valid(block_t const *block, block_t const *prev_block)
+{
+	uint8_t hash_buf[SHA256_DIGEST_LENGTH] = {0};
+	const block_t block_info = {
+	    create_block_info(),
+	    create_block_data(),
+	    "\xc5\x2c\x26\xc8\xb5\x46\x16\x39\x63\x5d\x8e\xdf\x2a\x97"
+	    "\xd4\x8d\x0c\x8e\x00\x09\xc8\x17\xf2\xb1\xd3\xd7\xff\x2f"
+	    "\x04\x51\x58\x03"};
+
+	if (!block || (!prev_block && block->info.index))
+		return (1);
+	if (block->info.index == 0)
+		return (memcmp(block, &block_info, sizeof(block_t)));
+	if (block->info.index != prev_block->info.index + 1)
+		return (1);
+	if (!block_hash(prev_block, hash_buf) ||
+	    memcmp(hash_buf, prev_block->hash, SHA256_DIGEST_LENGTH))
+		return (1);
+	if (memcmp(prev_block->hash, block->info.prev_hash, SHA256_DIGEST_LENGTH))
+		return (1);
+	if (!block_hash(block, hash_buf) ||
+	    memcmp(hash_buf, block->hash, SHA256_DIGEST_LENGTH))
+		return (1);
+	if (block->data.len > BLOCKCHAIN_DATA_MAX)
+		return (1);
+	return (0);
+}
