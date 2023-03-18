c420bc0b336f8de98e7e51e33e192a3fe49163d5
Ineffable22 <miguel_grillo_orellana@hotmail.com>
2023-03-18 00:21:06 -0500

    Feat(Update): Function removed and start with static values

--- blockchain/v0.1/block_is_valid.c
+++ blockchain/v0.1/block_is_valid.c
@@ -11,11 +11,21 @@ int block_is_valid(block_t const *block, block_t const *prev_block)
 {
 	uint8_t hash_buf[SHA256_DIGEST_LENGTH] = {0};
 	const block_t block_info = {
-	    create_block_info(),
-	    create_block_data(),
-	    "\xc5\x2c\x26\xc8\xb5\x46\x16\x39\x63\x5d\x8e\xdf\x2a\x97"
-	    "\xd4\x8d\x0c\x8e\x00\x09\xc8\x17\xf2\xb1\xd3\xd7\xff\x2f"
-	    "\x04\x51\x58\x03"};
+		{
+			0,	    /* index */
+			0,	    /* difficulty */
+			1537578000, /* timestamp */
+			0,	    /* nonce */
+			{0}	    /* prev_hash[SHA256_DIGEST_LENGTH] */
+		},
+		{
+			{0}, /* buffer */
+			16   /* len */
+		},
+		"\xc5\x2c\x26\xc8\xb5\x46\x16\x39\x63\x5d\x8e\xdf\x2a\x97"
+		"\xd4\x8d\x0c\x8e\x00\x09\xc8\x17\xf2\xb1\xd3\xd7\xff\x2f"
+		"\x04\x51\x58\x03"
+	};
 
 	if (!block || (!prev_block && block->info.index))
 		return (1);
