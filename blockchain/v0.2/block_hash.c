6ab029c24038ec1ce34ee33b8d2ca313964f7836
Ineffable22 <miguel_grillo_orellana@hotmail.com>
2023-03-23 17:25:00 -0500

    Feat(Update): remove conditional by return

--- blockchain/v0.2/block_hash.c
+++ blockchain/v0.2/block_hash.c
@@ -10,8 +10,6 @@
 uint8_t *block_hash(block_t const *block,
 		    uint8_t hash_buf[SHA256_DIGEST_LENGTH])
 {
-	if (!block || !hash_buf)
-		return (NULL);
 	if (!sha256((const int8_t *)&(block->info),
 		    sizeof(block->info) + block->data.len, hash_buf))
 		return (NULL);
