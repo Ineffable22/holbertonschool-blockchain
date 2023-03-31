ba8d97a84d42aa256be1b34f97493a581858948a
Ineffable22 <miguel_grillo_orellana@hotmail.com>
2023-03-31 09:44:44 -0500

    Feat(Update): Bug fixed

--- blockchain/v0.2/block_mine.c
+++ blockchain/v0.2/block_mine.c
@@ -1,4 +1,9 @@
 #include "blockchain.h"
+/* for ULONG_MAX */
+#include <limits.h>
+
+/* for memcpy */
+#include <string.h>
 
 /**
  * block_mine - Mines a Block in order to insert it in the Blockchain
@@ -6,9 +11,21 @@
  */
 void block_mine(block_t *block)
 {
-	block->info.nonce = 0;
-	do {
-		block->info.nonce += 1;
-		block_hash(block, block->hash);
-	} while (!hash_matches_difficulty(block->hash, block->info.difficulty));
+	uint8_t hash_buf[SHA256_DIGEST_LENGTH];
+	uint64_t i, nonce;
+
+	nonce = block->info.nonce;
+
+	for (i = 0; i < ULONG_MAX; i++)
+	{
+		block->info.nonce = i;
+
+		block_hash(block, hash_buf);
+		if (hash_matches_difficulty(hash_buf, block->info.difficulty))
+		{
+			memcpy(block->hash, hash_buf, SHA256_DIGEST_LENGTH);
+			return;
+		}
+	}
+	block->info.nonce = nonce;
 }
