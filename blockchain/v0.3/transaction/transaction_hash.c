99eb2edfc79f4ad450c00bf00b7a1c9a9295d812
Ineffable22 <miguel_grillo_orellana@hotmail.com>
2023-03-31 23:16:48 -0500

    Add: New file transaction/transaction_hash.c

---
+++ blockchain/v0.3/transaction/transaction_hash.c
@@ -0,0 +1,25 @@
+#include "transaction.h"
+
+/**
+ * transaction_hash - Computes the ID (hash) of a transaction
+ * @transaction: Points to the transaction to compute the hash of
+ * @hash_buf:    Is a buffer in which to store the computed hash
+ *
+ * Return: A pointer to hash_buf
+ */
+uint8_t *transaction_hash(transaction_t const *transaction,
+			  uint8_t hash_buf[SHA256_DIGEST_LENGTH])
+{
+	uint32_t len_in = 0, len_out = 0;
+
+	if (!transaction || !hash_buf)
+		return (NULL);
+	len_in = llist_size(transaction->inputs) * 3;
+	len_out = llist_size(transaction->outputs);
+	if (!len_in || !len_out)
+		return (NULL);
+	if (!sha256((int8_t *)transaction,
+		    (len_in + len_out) * SHA256_DIGEST_LENGTH, hash_buf))
+		return (NULL);
+	return (hash_buf);
+}
