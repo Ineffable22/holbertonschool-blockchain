e4e42acb4bce75a2dab9d285d40a3799b8d03897
Ineffable22 <miguel_grillo_orellana@hotmail.com>
2023-03-31 23:16:59 -0500

    Add: New file block_hash.c

---
+++ blockchain/v0.3/block_hash.c
@@ -0,0 +1,17 @@
+#include "blockchain.h"
+
+/**
+ * block_hash - Computes the hash of a Block
+ * @block:    Points to the Block to be hashed
+ * @hash_buf: The resulting hash must be stored in hash_buf
+ *
+ * Return: A pointer to hash_buf
+ */
+uint8_t *block_hash(block_t const *block,
+		    uint8_t hash_buf[SHA256_DIGEST_LENGTH])
+{
+	if (!sha256((const int8_t *)&(block->info),
+		    sizeof(block->info) + block->data.len, hash_buf))
+		return (NULL);
+	return (hash_buf);
+}
