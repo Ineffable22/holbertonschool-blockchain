b99222957575ca8cc9da95f8c02d928eeb173377
Ineffable22 <miguel_grillo_orellana@hotmail.com>
2023-04-04 20:30:27 -0500

    Feat(Update): Code Updated

--- blockchain/v0.3/block_is_valid.c
+++ blockchain/v0.3/block_is_valid.c
@@ -10,27 +10,12 @@
 int block_is_valid(block_t const *block, block_t const *prev_block)
 {
 	uint8_t hash_buf[SHA256_DIGEST_LENGTH] = {0};
-	block_t block_info = {
-		{
-			0,	    /* index */
-			0,	    /* difficulty */
-			1537578000, /* timestamp */
-			0,	    /* nonce */
-			{0}	    /* prev_hash[SHA256_DIGEST_LENGTH] */
-		},
-		{
-			"Holberton School",	/* buffer */
-			16			/* len */
-		},
-		"\xc5\x2c\x26\xc8\xb5\x46\x16\x39\x63\x5d\x8e\xdf\x2a\x97"
-		"\xd4\x8d\x0c\x8e\x00\x09\xc8\x17\xf2\xb1\xd3\xd7\xff\x2f"
-		"\x04\x51\x58\x03"
-	};
+	block_t const _genesis = GENESIS;
 
 	if (!block || (!prev_block && block->info.index != 0))
 		return (1);
 	if (block->info.index == 0)
-		return (memcmp(block, &block_info, sizeof(block_t)));
+		return (memcmp(block, &_genesis, sizeof(block_t)));
 	if (block->info.index != prev_block->info.index + 1)
 		return (1);
 	if (!block_hash(prev_block, hash_buf) ||
