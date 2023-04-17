8694112f2a5a3dcf01f5a29adc4ac57be33b8532
Ineffable22 <miguel_grillo_orellana@hotmail.com>
2023-04-17 00:34:00 -0500

    Add: New file serialize.c

---
+++ cli/serialize.c
@@ -0,0 +1,66 @@
+#include "cli.h"
+
+/**
+ * check - Check if the arguments are valid
+ * @arg:      Arguments
+ * @session:  Session struct
+ *
+ * Return: 0 if valid, 1 if not
+ */
+int check(char **arg, session_t *session)
+{
+	if (arg[1] != NULL)
+	{
+		session->state.code = 0;
+		session->state.msg = "Error: Path is NULL\n"
+				     "Usage: save <path>\n";
+		return (0);
+	}
+	if (arg[2] != NULL)
+	{
+		session->state.code = 0;
+		session->state.msg = "Error: too many arguments\n"
+				     "Usage: save <path>\n";
+		return (0);
+	}
+	if (session->blockchain == NULL)
+	{
+		session->state.code = 0;
+		session->state.msg = "Error: blockchain not loaded";
+		return (0);
+	}
+	if (session->wallet == NULL)
+	{
+		session->state.code = 0;
+		session->state.msg = "Error: wallet not loaded";
+		return (0);
+	}
+	if (llist_size(session->tx_pool) == 0)
+	{
+		session->state.code = 0;
+		session->state.msg = "Error: transaction pool empty";
+		return (0);
+	}
+	return (1);
+}
+
+/**
+ * serialize - Saves the blockchain
+ * @arg:      Arguments
+ * @session:  Session struct
+ *
+ * Return: Nothing
+ */
+void serialize(char **arg, session_t *session)
+{
+	if (check(arg, session) == 0)
+		return;
+	if (blockchain_serialize(session->blockchain, arg[1]))
+	{
+		session->state.code = 0;
+		session->state.msg = "Error: Failed to save blockchain";
+		return;
+	}
+	session->state.code = 0;
+	session->state.msg = "state: save";
+}
