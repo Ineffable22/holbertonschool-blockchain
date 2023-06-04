e5f71d050b1bbee42984109a29774aef3f1d3279
Ineffable22 <miguel_grillo_orellana@hotmail.com>
2023-06-04 17:43:47 -0500

    Feat(Update): Bug and errors fixed

--- cli/save.c
+++ cli/save.c
@@ -12,15 +12,15 @@ static int32_t check(char **arg, error_t *state)
 	if (arg[1] == NULL)
 	{
 		state->code = 0;
-		state->msg = "Error: missing file name\n"
-			     "Usage: wallet_save <file_name>\n";
+		state->msg = "Error: missing filename\n"
+			     "Usage: wallet_save <filename>\n";
 		return (1);
 	}
 	if (arg[2] != NULL)
 	{
 		state->code = 0;
 		state->msg = "Error: too many arguments\n"
-			     "Usage: wallet_save <file_name>\n";
+			     "Usage: wallet_save <filename>\n";
 		return (1);
 	}
 	return (0);
@@ -37,6 +37,7 @@ void save(char **arg, session_t *session)
 {
 	EC_KEY *key = NULL;
 	uint8_t pub[EC_PUB_LEN] = {0};
+	uint32_t balance = 0;
 
 	if (check(arg, &session->state))
 		return;
@@ -64,9 +65,17 @@ void save(char **arg, session_t *session)
 	_print_hex_buffer(pub, EC_PUB_LEN);
 	printf("\nSaved in %s directory\n", arg[1]);
 	session->state.code = 0;
-	session->state.msg = "state: save";
+	session->state.msg = "Session saved";
 	if (!session->wallet)
 		session->wallet = calloc(1, sizeof(wallet_t));
 	memcpy(session->wallet->pub, pub, EC_PUB_LEN);
+	session->wallet->username = arg[1];
+	balance = get_balance(session->blockchain->unspent, session->wallet->pub);
+	session->wallet->balance = balance;
 	session->wallet->key = key;
+	printf("Public 2: ");
+	ec_to_pub(session->wallet->key, pub);
+	_print_hex_buffer(pub, EC_PUB_LEN);
+	putchar('\n');
+	EC_KEY_free(key);
 }
